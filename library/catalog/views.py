f2185e6b5309db05b1204c151e66b80624dbcb4b
Lowic Mangin <loic.mangin@ipsosante.fr>
2020-04-27 17:52:40 +0200

    Add a simple view and template for Catalog home page

--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import render
+
+from .models import Author, Book, BookInstance, Genre, Language
 
 
-def catalog_home(request):
-    return HttpResponse("Hello Catalog!")
+
+def index(request):
+    authors_count = Author.objects.all().count()
+    books_count = Book.objects.all().count()
+    instances_count = BookInstance.objects.all().count()
+
+    available_instances_count = BookInstance.objects.filter(status__exact='a').count()
+
+    context = {
+        'authors_count': authors_count,
+        'books_count': books_count,
+        'instances_count': instances_count,
+        'available_instances_count': available_instances_count,
+    }
+
+    return render(request, 'index.html', context=context)
